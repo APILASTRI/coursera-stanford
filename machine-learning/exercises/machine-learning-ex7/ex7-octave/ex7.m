452cfed51349087195b99ec859bf2ee99d2032ff
thiagoPanini <thipanini94@gmail.com>
2019-04-25 23:54:20 -0300

    Exercício 7 - KMeans em Python

--- machine-learning/exercises/machine-learning-ex7/ex7-octave/ex7.m
+++ machine-learning/exercises/machine-learning-ex7/ex7-octave/ex7.m
@@ -34,6 +34,8 @@ load('ex7data2.mat');
 % Select an initial set of centroids
 K = 3; % 3 Centroids
 initial_centroids = [3 3; 6 2; 8 5];
+initial_centroids
+size(initial_centroids)
 
 % Find the closest centroids for the examples using the
 % initial_centroids
